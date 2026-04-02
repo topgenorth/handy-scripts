afa7b711da40de95cb57b83dc7371f2258ebfcff
Tom Opgenorth <tom@opgenorth.net>
2026-04-01 21:45:25 -0600

    Install powershell, update vs-codeextensions

--- vscode-extensions.sh
+++ vscode-extensions.sh
@@ -1,10 +1,10 @@
 code --install-extension golang.go
-code --install-extension eamodio.errorlens
 
 code --install-extension ms-python.python
 code --install-extension ms-python.vscode-pylance
 code --install-extension esbenp.prettier-vscode
 
+code --install-extension frontmatter.frontmatter
 code --install-extension yzhang.markdown-all-in-one
 code --install-extension davidanson.vscode-markdownlint
 code --install-extension streetsidesoftware.code-spell-checker
@@ -12,14 +12,15 @@ code --install-extension streetsidesoftware.code-spell-checker
 code --install-extension GitHub.vscode-pull-request-github
 
 code --install-extension eamodio.gitlens
-#code --install-extension GitHub.copilot
 
 code --install-extension phoenisx.gohugo
 code --install-extension eliostruyf.vscode-hugo-themer
 code --install-extension eliostruyf.vscode-front-matter
 
-code --install-extension frontmatter.frontmatter
-
 code --install-extensions erlang-ls.erlang-ls
 
 code --install-extensions jetbrains.resharper-code
+
+code --install-extensions foxundermoon.shell-format
+
+code --install-extensions ms-vscode.powershell
