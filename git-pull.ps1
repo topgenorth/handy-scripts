f0199cf7d0e529e1ed86f0cc752bce385f01a1c5
Tom Opgenorth <tom@opgenorth.net>
2026-04-23 19:44:52 -0600

    First check git status.

--- git-pull.ps1
+++ git-pull.ps1
@@ -1,19 +1,26 @@
-#!/usr/bin/env pwsh
-
 $repos = @(
-    "Dir1",
-    "Dir2"
+    "~/Dir1",
+    "~/Dir2"
 )
 
 foreach ($dir in $repos) {
     $name = Split-Path $dir -Leaf
     Write-Host "=== $name ==="
 
-    if (Test-Path (Join-Path $dir ".git")) {
-        git -C $dir pull
+    if (-not (Test-Path (Join-Path $dir ".git"))) {
+        Write-Host "Not a git repository: $dir"
+        Write-Host ""
+        continue
+    }
+
+    $status = git -C $dir status --porcelain
+
+    if ([string]::IsNullOrWhiteSpace(($status | Out-String))) {
+        Write-Host "No local changes detected, skipping git pull"
     }
     else {
-        Write-Host "Not a git repository: $dir"
+        Write-Host "Changes detected, running git pull"
+        git -C $dir pull
     }
 
     Write-Host ""
