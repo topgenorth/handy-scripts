0333c8872b179e20ae3d2a6a272d3d121416d168
Tom Opgenorth <tom@opgenorth.net>
2026-07-28 15:34:54 -0600

    Install WezTerm

---
+++ install-wezterm.sh
@@ -0,0 +1,11 @@
+# Add GPG key
+curl -fsSL https://apt.fury.io/wez/gpg.key | \
+  sudo gpg --yes --dearmor -o /usr/share/keyrings/wezterm-fury.gpg
+
+# Add repo
+echo 'deb [signed-by=/usr/share/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | \
+  sudo tee /etc/apt/sources.list.d/wezterm.list
+
+# Install
+sudo apt update
+sudo apt install wezterm
